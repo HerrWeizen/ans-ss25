e530b8f0f814da8569872ad97c39932955b27e56
HerrWeizen <johannesdorfschmidt@gmail.com>
2025-05-07 11:42:49 +0200

    i dont have a f clue

--- lab1/ans_controller.py
+++ lab1/ans_controller.py
@@ -24,7 +24,7 @@ from ryu.controller import ofp_event
 from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
-from ryu.lib.packet import ethernet, arp, packet # https://ryu.readthedocs.io/en/latest/library_packet.html
+from ryu.lib.packet import ethernet, arp, ipv4, ether_types, packet # https://ryu.readthedocs.io/en/latest/library_packet.html
 
 
 class LearningSwitch(app_manager.RyuApp):
@@ -35,6 +35,22 @@ class LearningSwitch(app_manager.RyuApp):
 
         # Tabelle für MAC/Ports
         self.mac_to_port = {}
+
+        #used to detect whether a paket is directed to the router or not
+        self.detect_router = {"00:00:00:00:01:01","00:00:00:00:01:02","00:00:00:00:01:03"}
+
+        # Router port MACs assumed by the controller
+        self.port_to_own_mac = {
+        1: "00:00:00:00:01:01",
+        2: "00:00:00:00:01:02",
+        3: "00:00:00:00:01:03"
+        }
+        # Router port (gateways) IP addresses assumed by the controller
+        self.port_to_own_ip = {
+        1: "10.0.1.1",
+        2: "10.0.2.1",
+        3: "192.168.1.1"
+        }
         
 
     # Wird aufgerufen wenn sich die switch nach dem Verbindungsaufbau beim Controller meldet 
@@ -66,12 +82,32 @@ class LearningSwitch(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev): # wtf is ev?
         self.logger.info(f"Received message type: {type(ev.msg)}")
-        print(type(ev.msg))
+
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
 
         # parse raw packet into a structure object - .data extracts the raw data from the msg - contains all protocol parts
+        pkt = packet.Packet(msg.data)
+
+        # extract ethernet layer form parsed object
+        eth = pkt.get_protocol(ethernet.ethernet) 
+
+        # ethernet destination and source as MAC-addresses (strings)
+        dst_MAC = eth.dst
+        if dst_MAC not in self.detect_router:
+            self.switch_logic(msg)
+        else:
+            if eth.ethertype == ether_types.ETH_TYPE_ARP:
+                self.handle_router_arp(msg)
+            elif eth.ethertype == ether_types.ETH_TYPE_IP:
+                self.handle_router_ip(msg)
+
+    def switch_logic(self, msg):
+
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+    
         pkt = packet.Packet(msg.data)
 
         # extract ethernet layer form parsed object
@@ -90,7 +126,8 @@ class LearningSwitch(app_manager.RyuApp):
         
         # create the MAC - Port entry#
         in_port = msg.match['in_port']
-        self.mac_to_port[dpid][src_MAC] = in_port
+        if src_MAC not in self.mac_to_port[dpid]:
+            self.mac_to_port[dpid][src_MAC] = in_port
 
         # look if the destination is present in the mac - port table for the switch, if not flood all ports
         if dst_MAC in self.mac_to_port[dpid]:
@@ -114,3 +151,89 @@ class LearningSwitch(app_manager.RyuApp):
         
         # Datapath sagen, schick den scheiß
         datapath.send_msg(forward)
+
+    def router_arp_logic(self, msg):
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+        arp_pkt = pkt.get_protocol(arp.arp)
+
+        # Check if ARP request is for one of the router's IPs
+        if arp_pkt.opcode == arp.ARP_REQUEST:
+            for port, ip in self.port_to_own_ip.items():
+                if arp_pkt.dst_ip == ip:
+                    # Build ARP reply
+                    reply = packet.Packet()
+                    reply.add_protocol(ethernet.ethernet(
+                        ethertype=eth.ethertype,
+                        dst=eth.src,
+                        src=self.port_to_own_mac[port]
+                    ))
+                    reply.add_protocol(arp.arp(
+                        opcode=arp.ARP_REPLY,
+                        src_mac=self.port_to_own_mac[port],
+                        src_ip=ip,
+                        dst_mac=arp_pkt.src_mac,
+                        dst_ip=arp_pkt.src_ip
+                    ))
+                    # Send reply back
+                    self._send_packet(msg.datapath, msg.match['in_port'], reply)
+                    break
+
+    def router_ip_logic(self, msg):
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        # Parse Ethernet and IP packet
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocol(ethernet.ethernet)
+        ip_pkt = pkt.get_protocol(ipv4.ipv4)
+
+        if not ip_pkt:
+            return  # Not an IPv4 packet, ignore
+
+        dst_ip = ip_pkt.dst
+        src_ip = ip_pkt.src
+
+        # Check which router port the packet arrived on
+        incoming_subnet = None
+        for port, ip in self.port_to_own_ip.items():
+            if ip.split('.')[0:3] == dst_ip.split('.')[0:3]:  # Check /24 subnet
+                incoming_subnet = port
+                break
+
+        # Case 1: Destination IP is in one of the router's subnets
+        if incoming_subnet:
+            # Forward to the correct port (e.g., 10.0.1.2 -> Port 1)
+            out_port = incoming_subnet
+            actions = [parser.OFPActionOutput(out_port)]
+
+        # Case 2: Destination IP is external (e.g., Internet)
+        else:
+            # Default route (e.g., send to Port 3 as gateway)
+            out_port = 3  # Assume Port 3 connects to the Internet
+            actions = [parser.OFPActionOutput(out_port)]
+
+        # Rewrite MAC addresses (Router replaces src/dst MAC)
+        new_eth = ethernet.ethernet(
+            src=self.port_to_own_mac[out_port],  # Router's MAC for the out_port
+            dst=self._get_dst_mac_for_ip(dst_ip),  # Requires ARP cache (see below)
+            ethertype=eth.ethertype
+        )
+
+        # Rebuild the packet with new Ethernet header
+        new_pkt = packet.Packet()
+        new_pkt.add_protocol(new_eth)
+        new_pkt.add_protocol(ip_pkt)  # Keep original IP packet
+        new_pkt.serialize()
+
+        # Send the packet out
+        self._send_packet(datapath, out_port, new_pkt)
+
+        # Install a flow to avoid future packet-ins
+        match = parser.OFPMatch(
+            eth_type=ether_types.ETH_TYPE_IP,
+            ipv4_dst=dst_ip
+        )
+        self.add_flow(datapath, 2, match, actions)
