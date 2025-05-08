b49278bf5343cd1f365bdc3f80c3cce9367cf4f5
HerrWeizen <johannesdorfschmidt@gmail.com>
2025-05-08 15:43:01 +0200

    sind ja keywords

--- lab1/ans_controller.py
+++ lab1/ans_controller.py
@@ -146,8 +146,8 @@ class LearningSwitch(app_manager.RyuApp):
         return_mac = eth_pkt_in.src # the MAC of the last hop
 
         eth_reply = ethernet.ethernet(
-            src_mac = requested_mac, # the router is currently sending
-            dst_mac = return_mac, # send it to the last hop
+            src = requested_mac, # the router is currently sending
+            dst = return_mac, # send it to the last hop
             ethertype = eth_pkt_in.ethertype 
         )
 
